281b5ed9ad7d71ecdc2687bfa81207ded5bad675
viktor <viktorrabota22@gmail.com>
2016-05-16 19:08:20 +0300

    adding superman

--- TestNotification/TestNotification/AppDelegate.m
+++ TestNotification/TestNotification/AppDelegate.m
@@ -12,7 +12,9 @@
 #import "SVPensionPerson.h"
 #import "SVBusinesPerson.h"
 
-@interface AppDelegate ()
+@interface AppDelegate (){
+    NSNotificationCenter *nc;
+}
 
 @property(strong, nonatomic)SVGovernment *government;
 @property(strong, nonatomic)NSArray *people;
@@ -23,6 +25,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    nc = [NSNotificationCenter defaultCenter];
+    
+    [nc addObserver:self selector:@selector(applicationWillResignActiveNotification:) name:UIApplicationWillResignActiveNotification object:nil];
+    [nc addObserver:self selector:@selector(applicationDidEnterBackgroundNotification:) name:UIApplicationDidEnterBackgroundNotification object:nil];
+    [nc addObserver:self selector:@selector(applicationWillEnterForegroundNotification:) name:UIApplicationWillEnterForegroundNotification object:nil];
+    [nc addObserver:self selector:@selector(applicationDidBecomeActiveNotification:) name:UIApplicationDidBecomeActiveNotification object:nil];
+    [nc addObserver:self selector:@selector(applicationWillTerminateNotification:) name:UIApplicationWillTerminateNotification object:nil];
     
     _people = [[NSArray alloc]init];
     _government = [[SVGovernment alloc] init];
@@ -50,30 +60,57 @@
     _government.salary = _government.salary + arc4random()%100;
     _government.pension = _government.pension + arc4random()%100;
     _government.tax = _government.tax + arc4random()%100;
+    
+
+    
 
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    NSLog(@"It's original function %s",__PRETTY_FUNCTION__);
 }
+-(void)applicationWillResignActiveNotification:(NSNotification *)notification{
+    NSLog(@"It's notification function %s", __PRETTY_FUNCTION__);
+}
+
+
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    NSLog(@"It's original function AppDelegate %s",__PRETTY_FUNCTION__);
 }
+- (void)applicationDidEnterBackgroundNotification:(NSNotification *)notification {
+   NSLog(@"It's notification function %s", __PRETTY_FUNCTION__);
+}
+
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    NSLog(@"It's original function AppDelegate %s",__PRETTY_FUNCTION__);
 }
+- (void)applicationWillEnterForegroundNotification:(NSNotification *)notification {
+    NSLog(@"It's notification function %s", __PRETTY_FUNCTION__);
+}
+
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    NSLog(@"It's original function AppDelegate %s",__PRETTY_FUNCTION__);
+}
+- (void)applicationDidBecomeActiveNotification:(NSNotification *)notification {
+    NSLog(@"It's notification function %s", __PRETTY_FUNCTION__);
 }
 
+
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSLog(@"It's original function AppDelegate %s",__PRETTY_FUNCTION__);
+}
+- (void)applicationWillTerminateNotification:(NSNotification *)notification {
+    NSLog(@"It's notification function %s", __PRETTY_FUNCTION__);
+}
+
+
+- (void)dealloc
+{
+    [nc removeObserver:self];
 }
 
 @end
