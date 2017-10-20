60d841676f096c3d9d631e3820f8e9c89856fae1
viktor <viktorrabota22@gmail.com>
2017-10-20 19:56:17 +0300

    add autolayout code: case VFL and case Constraint

--- SVCalcAutoLayoutCode/SVCalcAutoLayoutCode/SVMainViewController.m
+++ SVCalcAutoLayoutCode/SVCalcAutoLayoutCode/SVMainViewController.m
@@ -44,19 +44,21 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
+    
+    self.view.translatesAutoresizingMaskIntoConstraints = NO;
+    self.labelScreen.translatesAutoresizingMaskIntoConstraints = NO;
+    self.viewButtons.translatesAutoresizingMaskIntoConstraints = NO;
+    
     if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 9.0) {
         [self makeAutoLayoutWithAnchors];
     }else{
         [self makeAutoLAyoutWithConstrains];
+        //[self makeAutoLayoutWithVFL];
     }
     
 }
 
 -(void)makeAutoLayoutWithAnchors{
-    self.view.translatesAutoresizingMaskIntoConstraints = NO;
-    self.labelScreen.translatesAutoresizingMaskIntoConstraints = NO;
-    self.viewButtons.translatesAutoresizingMaskIntoConstraints = NO;
-    
     UILayoutGuide *margineGuide = self.view.layoutMarginsGuide;
     [self.labelScreen.leadingAnchor constraintEqualToAnchor:margineGuide.leadingAnchor].active = YES;
     [self.labelScreen.trailingAnchor constraintEqualToAnchor:margineGuide.trailingAnchor].active = YES;
@@ -66,10 +68,88 @@
     [self.viewButtons.leadingAnchor constraintEqualToAnchor:margineGuide.leadingAnchor].active = YES;
     [self.viewButtons.trailingAnchor constraintEqualToAnchor:margineGuide.trailingAnchor].active = YES;
     [self.viewButtons.bottomAnchor constraintEqualToAnchor:margineGuide.bottomAnchor constant:-20].active = YES;
-    [self.viewButtons.heightAnchor constraintEqualToConstant:CGRectGetWidth(self.view.frame)+40].active = YES;
+    [self.viewButtons.heightAnchor constraintEqualToConstant:CGRectGetWidth(self.view.frame)+45].active = YES;
+}
+
+-(void)makeAutoLayoutWithVFL{
+    NSDictionary *viewsDictionary = @{@"viewButtons":self.viewButtons,
+                                      @"labelScreen":self.labelScreen};
+    NSDictionary *metrics = @{@"space":@(20),
+                              @"sizeViewHeight":@(CGRectGetWidth(self.view.frame)+35)};
+    NSArray *horizontalConstrains1 = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[labelScreen]-20-|" options:0 metrics:nil views:viewsDictionary];
+    NSArray *horizontalConstrains2 = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[viewButtons]-20-|" options:0 metrics:nil views:viewsDictionary];
+    NSArray *verticalConstrains1 = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-space-[labelScreen]-0-[viewButtons(sizeViewHeight)]-space-|" options:0 metrics:metrics views:viewsDictionary];
+    [self.view addConstraints:horizontalConstrains1];
+    [self.view addConstraints:horizontalConstrains2];
+    [self.view addConstraints:verticalConstrains1];
+    //but if need change multiplier, you will need add Constraint with this parametr
 }
 
 -(void)makeAutoLAyoutWithConstrains{
+    [NSLayoutConstraint constraintWithItem:self.labelScreen
+                                 attribute:NSLayoutAttributeLeading
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.view
+                                 attribute:NSLayoutAttributeLeading
+                                multiplier:1
+                                  constant:20].active = YES;
+    
+    [NSLayoutConstraint constraintWithItem:self.labelScreen
+                                 attribute:NSLayoutAttributeTrailing
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.view
+                                 attribute:NSLayoutAttributeTrailing
+                                multiplier:1
+                                  constant:-20].active = YES;
+    
+    [NSLayoutConstraint constraintWithItem:self.labelScreen
+                                 attribute:NSLayoutAttributeTop
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.view
+                                 attribute:NSLayoutAttributeTop
+                                multiplier:1
+                                  constant:20].active = YES;
+    
+    [NSLayoutConstraint constraintWithItem:self.labelScreen
+                                 attribute:NSLayoutAttributeBottom
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.viewButtons
+                                 attribute:NSLayoutAttributeTop
+                                multiplier:1
+                                  constant:0].active = YES;
+    
+    
+    [NSLayoutConstraint constraintWithItem:self.viewButtons
+                                 attribute:NSLayoutAttributeLeading
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.view
+                                 attribute:NSLayoutAttributeLeading
+                                multiplier:1
+                                  constant:20].active = YES;
+    
+    [NSLayoutConstraint constraintWithItem:self.viewButtons
+                                 attribute:NSLayoutAttributeTrailing
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.view
+                                 attribute:NSLayoutAttributeTrailing
+                                multiplier:1
+                                  constant:-20].active = YES;
+    
+    [NSLayoutConstraint constraintWithItem:self.viewButtons
+                                 attribute:NSLayoutAttributeBottom
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.view
+                                 attribute:NSLayoutAttributeBottom
+                                multiplier:1
+                                  constant:-20].active = YES;
+    
+    [NSLayoutConstraint constraintWithItem:self.viewButtons
+                                 attribute:NSLayoutAttributeHeight
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:nil
+                                 attribute:NSLayoutAttributeNotAnAttribute
+                                multiplier:1
+                                  constant:CGRectGetWidth(self.view.frame)+35].active = YES;
     
 }
 
